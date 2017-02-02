add68bb609b54214b0cf258b018dc75aafb4b154
funkybo <esher5580@gmail.com>
2017-02-02 17:59:10 +0200

    main page products page

---
+++ resources/views/main.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <div class="container">
+            <h1>Hello, guys!</h1>
+            <p>This is my first primitive web-site on Laravel framework. Link to official web-page below.</p>
+            <p><a class="btn btn-primary btn-lg" href="https://laravel.com/" role="button">Learn more »</a></p>
+        </div>
+    </div>
+
+    <div class="col-xs-12">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus adipisci atque blanditiis, delectus
+            dolores esse eum fugiat impedit in inventore ipsam labore laboriosam minima molestiae non placeat porro
+            possimus quo ratione sint tempora temporibus, veritatis voluptate voluptatem. Consectetur dolores ea eveniet
+            fugit id maxime neque pariatur, porro praesentium qui quidem similique tempora vel. Amet at autem, cum iste
+            libero neque officia voluptates! Aliquid asperiores, eos esse, ex, fuga libero maxime numquam officiis
+            pariatur repellat tenetur totam! Aperiam cupiditate dolorem recusandae suscipit voluptatem! Ad amet at,
+            blanditiis commodi culpa, cumque, dignissimos earum eius est fugiat fugit harum hic minus omnis qui
+            quibusdam rem reprehenderit saepe sint suscipit tempora unde veniam? Distinctio est fuga harum inventore
+            ipsum iure, magni nam nihil non odio odit quo sapiente soluta? Distinctio harum iure maxime voluptate.
+            Aspernatur expedita quibusdam sint ut! Dolorem dolores eius eum eveniet excepturi facere maiores molestiae
+            nulla provident vitae? Accusantium aliquam at blanditiis deserunt error, excepturi explicabo laudantium nemo
+            non numquam officia provident repudiandae sapiente tempora tenetur vel voluptate! Illo natus nihil non rerum
+            vero! At doloribus et quam quos vel. Culpa delectus, deserunt enim facere in inventore iure maiores maxime,
+            minima molestias porro tempora totam veritatis. Animi deserunt qui rerum?</p>
+    </div>
+@stop
---
+++ resources/views/products.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    @foreach($products as $product)
+        <div class="col-md-4">
+            <div class="img-box">
+                <img src="{{ $product->img_url }}" alt="" class="img-responsive">
+            </div>
+            <h2>{{ $product->title }}</h2>
+            <div style="min-height: 140px;">
+                <p>{{ $product->description }}</p>
+            </div>
+            <h4>Price: £{{ $product->price }}</h4>
+            <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+        </div>
+    @endforeach
+@stop
